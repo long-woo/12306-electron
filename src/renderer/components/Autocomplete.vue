d91d5baad009df500fbbd7529e042a15c318ff9d
long.woo <longwunet@gmail.com>
2017-09-18 23:12:08 +0800

    autocomplete组件增加属性和方法

--- src/renderer/components/Autocomplete.vue
+++ src/renderer/components/Autocomplete.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <b-form-input ></b-form-input>
-    <div class="dropdown-menu">
-     <b-dropdown-item></b-dropdown-item>
+    <b-form-input v-model="selectText"></b-form-input>
+    <div class="dropdown-menu" :class="{'show': show}">
+      <b-dropdown-header>{{dropdownHeader}}</b-dropdown-header>
+      <b-dropdown-item :class="{'active': isActive(index)}" v-for="(item, index) in dropdownData" :key="index" @click="itemClick(index)">{{item.text}}</b-dropdown-item>
     </div>
   </div>
 </template>
@@ -12,7 +13,24 @@ export default {
   name: 'bAutocomplete',
   data () {
     return {
-
+      show: false,
+      selectIndex: 0,
+      selectText: this.dropdownData[0]
+    }
+  },
+  props: {
+    dropdownHeader: String,
+    dropdownData: Array,
+    selectValue: String
+  },
+  methods: {
+    isActive (index) {
+      return index === this.selectIndex
+    },
+    itemClick (index) {
+      this.selectIndex = index
+      this.selectData = this.dropdownData[index]
+      this.show = false
     }
   }
 }
