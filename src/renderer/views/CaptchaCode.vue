476cd7a51f1dc0822c357e095cced2bff9bdfec8
long.woo <longwunet@gmail.com>
2017-10-17 21:45:48 +0800

    将验证码图片画到画布中

--- src/renderer/views/CaptchaCode.vue
+++ src/renderer/views/CaptchaCode.vue
@@ -1,7 +1,7 @@
 <template>
   <b-modal id="captchCodeModal" :title="title" :hideHeaderClose="true" @change="change">
     <div class="text-center img-captcha">
-      <img :src="imgCaptchaCode" class="img-fluid" />
+      <canvas id="cvCaptchaCode" width="293px" height="190px" @click="selectCode"></canvas>
     </div>
     <b-button slot="modal-footer" variant="success" class="waves-effect" @click="getCaptchaCode">换一张？</b-button>
     <b-button slot="modal-footer" variant="secondary" class="waves-effect" @click="cancel">返回</b-button>
@@ -12,6 +12,10 @@
 <style scoped>
 .img-captcha {
   height: 190px;
+}
+
+.img-captcha canvas {
+  box-shadow: 0 6px 20px rgba(225, 225, 225, 1)
 }
 </style>
 
@@ -33,12 +37,28 @@ export default {
     // 获取验证码
     async getCaptchaCode () {
       const res = await this.$api.getCaptchaCode()
-      this.imgCaptchaCode = res
+      const img = new Image()
+      const canvas = document.getElementById('cvCaptchaCode')
+      const context = canvas.getContext('2d')
+
+      context.fillStyle = '#fff'
+      context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)
+      context.fill()
+      img.src = res
+      img.onload = () => {
+        context.drawImage(img, 0, 0)
+      }
     },
     change (state) {
       if (state) {
         this.getCaptchaCode()
       }
+    },
+    // 选择验证码
+    selectCode (e) {
+      console.log(e)
+      const chkImg = new Image()
+      chkImg.src = require('@/assets/icon_like.png')
     },
     // 返回
     cancel () {
